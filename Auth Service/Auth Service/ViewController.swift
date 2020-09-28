e21bdb403ab5766347511c922b8ed2f7ac3415ed
Emre AYDIN <ea@ea.tc>
2020-09-28 12:48:39 +0300

    Project created..

---
+++ Auth Service/Auth Service/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auth Service
+//
+//  Created by Emre AYDIN on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
